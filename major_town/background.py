b8f9795789fa1a0cc5e76d28c04e97b9770c9c35
Allan Soares <coallanvei@gmail.com>
2019-11-16 02:40:35 -0300

    Using .convert() when loading the background image to improve the performance

--- major_town/background.py
+++ major_town/background.py
@@ -4,14 +4,14 @@ class Background(pygame.sprite.Sprite):
     
     def __init__(self, image_file, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image1 = pygame.image.load(image_file)
+        self.image1 = pygame.image.load(image_file).convert()
         self.rect1 = self.image1.get_rect()
         self.rect1.left = 0
         self.rect1.top = 0
         self.width1 = self.image1.get_width() 
         self.height1 = self.image1.get_height() 
 
-        self.image2 = pygame.image.load(image_file)
+        self.image2 = pygame.image.load(image_file).convert()
         self.rect2 = self.image2.get_rect()
         self.rect2.left = 0
         self.rect2.top = -self.height1
--- major_town/bullet.py
+++ major_town/bullet.py
@@ -4,7 +4,7 @@ class Bullet:
 
     def __init__(self, image_file, location, size):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
+        self.image = pygame.image.load(image_file).convert()
         self.image = pygame.transform.scale(self.image, size)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
