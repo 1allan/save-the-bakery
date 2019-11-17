75919a658a0bba49bb1436e37b3d7130ed6ba350
Allan Soares <coallanvei@gmail.com>
2019-11-17 01:41:46 -0300

    Fixing infinit background. It's provisory

--- save-the-bakery/Background.py
+++ save-the-bakery/Background.py
@@ -12,6 +12,7 @@ class Background(pygame.sprite.Sprite):
         self.height1 = self.image1.get_height() 
 
         self.image2 = pygame.image.load(image_file).convert()
+        self.image2 = pygame.transform.scale(self.image2, (self.image1.get_width(), self.image1.get_height()))
         self.rect2 = self.image2.get_rect()
         self.rect2.left = 0
         self.rect2.top = -self.height1
@@ -23,9 +24,11 @@ class Background(pygame.sprite.Sprite):
         self.rect2.top += 1
         self.rect1.top += 1
 
-        if self.rect1.top > screen.get_height() - 50:
-            self.rect1.top = 0
-            self.rect2.top = -self.height1
+        if self.rect1.top >= screen.get_height():
+            self.rect1.top = -self.height1
+            
+        if self.rect2.top >= screen.get_height():
+            self.rect2.top = -self.height2
         
         screen.blit(self.image1, self.rect1)
         screen.blit(self.image2, self.rect2)
